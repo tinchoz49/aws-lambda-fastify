08eb651028d9412248e4159aa96c685c33ce91f3
Martín Acosta <tinchoz49@gmail.com>
2022-06-23 13:09:04 -0300

    delete unused files

--- req-res.js
+++ req-res.js
@@ -1,12 +1,61 @@
 const { EventEmitter } = require('events')
 const { Readable, Writable } = require('streamx')
-const parseURL = require('./parse-url')
+
+const BASE_URL = 'http://localhost'
+
+function parseURL (url, query) {
+  if ((typeof url === 'string' || Object.prototype.toString.call(url) === '[object String]') && url.startsWith('//')) {
+    url = BASE_URL + url
+  }
+  const result = typeof url === 'object'
+    ? Object.assign(new URL(BASE_URL), url)
+    : new URL(url, BASE_URL)
+
+  const merged = Object.assign({}, url.query, query)
+  for (const key in merged) {
+    const value = merged[key]
+
+    if (Array.isArray(value)) {
+      result.searchParams.delete(key)
+      for (const param of value) {
+        result.searchParams.append(key, param)
+      }
+    } else {
+      result.searchParams.set(key, value)
+    }
+  }
+
+  return result
+}
 
 class MockSocket extends EventEmitter {
   constructor (remoteAddress) {
     super()
     this.remoteAddress = remoteAddress
   }
+}
+
+function hostHeaderFromURL (parsedURL) {
+  return parsedURL.port
+    ? parsedURL.host
+    : parsedURL.hostname + (parsedURL.protocol === 'https:' ? ':443' : ':80')
+}
+
+let utcCache
+
+function utcDate () {
+  if (!utcCache) cache()
+  return utcCache
+}
+
+function cache () {
+  const d = new Date()
+  utcCache = d.toUTCString()
+  setTimeout(resetCache, 1000 - d.getMilliseconds())
+}
+
+function resetCache () {
+  utcCache = undefined
 }
 
 class QueryRefs {
@@ -38,21 +87,7 @@ class QueryRefs {
   }
 }
 
-const queryRefs = new QueryRefs()
-
-exports.queryRefs = queryRefs
-
-/**
- * Get hostname:port
- *
- * @param {URL} parsedURL
- * @return {String}
- */
-function hostHeaderFromURL (parsedURL) {
-  return parsedURL.port
-    ? parsedURL.host
-    : parsedURL.hostname + (parsedURL.protocol === 'https:' ? ':443' : ':80')
-}
+const queryRefs = exports.queryRefs = new QueryRefs()
 
 exports.Request = class Request extends Readable {
   constructor (opts = {}) {
@@ -103,23 +138,6 @@ exports.Request = class Request extends Readable {
     this.push(null)
     cb(null)
   }
-}
-
-let utcCache
-
-function utcDate () {
-  if (!utcCache) cache()
-  return utcCache
-}
-
-function cache () {
-  const d = new Date()
-  utcCache = d.toUTCString()
-  setTimeout(resetCache, 1000 - d.getMilliseconds())
-}
-
-function resetCache () {
-  utcCache = undefined
 }
 
 exports.Response = class Response extends Writable {
